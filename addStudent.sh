2f8dc4fc60df18d9b0b7c00cc8dff686259ab598
JustinJmnz <jjimenez@my.bridgeport.edu>
2015-11-23 19:33:45 -0500

    Deleted prof_mailer

--- addStudent.sh
+++ addStudent.sh
@@ -1,8 +1,29 @@
 checkDuplicates() {
-studentIDs=`awk -F ":" '{print $3}' $file`
-studentEmails=`awk -F ":" '{print $4}' $file`
-echo $studentIDs
-echo $studentEmails
+studentIDs=`awk -F ":" '{print $3}' $file` # Get 3rd field
+echo $studentIDs > tmpStudentIDs # Print all field 3s to a file
+tr ' ' '\n' < tmpStudentIDs > tmp # Replace spaces with \n to tmp
+mv tmp tmpStudentIDs #rename tmp to tmpStudentIDs
+studentEmails=`awk -F ":" '{print $4}' $file` # Get 4th field
+echo $studentEmails > tmpStudentEmails # Print all field 4s to a file
+tr ' ' '\n' < tmpStudentEmails > tmp  # Replace spaces with \n
+mv tmp tmpStudentEmails # Rename tmp to tmpStudentEmails
+lastID=`sed -n '$p' tmpStudentIDs`
+lastEmail=`sed -n '$p' tmpStudentEmails`
+occID=`grep -i $lastID tmpStudentIDs | wc -l`
+occEmail=`grep -i $lastEmail tmpStudentEmails | wc -l`
+echo $occID
+echo $occEmail
+if [ $occID > 1 ] || [ $occEmail > 1 ]; then # If file contains $lastID
+	echo `sed -n '$p' tmpStudentIDs`
+	echo "ID/Email exists already, add student?"
+	echo "Yes: 1"
+	echo "No: 2"
+	read choice
+	if [ $choice == 2 ]; then
+		sed '$d' < $file > tmp # Delete last line save to tmp
+		mv tmp $file # rename tmp to $file
+	fi
+fi	
 }
 addStudentFIELDS() {
 echo "Enter student last name:"
